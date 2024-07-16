4ac947fc5a2b3a468c9023dfe2900a0d1f16b08f
Erica Sampson <ericabcherry@gmail.com>
2024-07-16 09:11:02 -0400

    Initial Commit

---
+++ 100DS-P5-WordScramble/_00DS_P5_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DS_P5_WordScrambleApp.swift
+//  100DS-P5-WordScramble
+//
+//  Created by Erica Sampson on 2024-07-16.
+//
+
+import SwiftUI
+
+@main
+struct _00DS_P5_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
